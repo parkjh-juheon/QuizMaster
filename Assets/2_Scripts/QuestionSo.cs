ef6e8f57866bb712f6a5986108d55b19680b17c6
shw-jpg <vahyeon2501@sonline20.sen.go.kr>
2025-08-14 11:39:00 +0900

    for

--- Assets/2_Scripts/QuestionSo.cs
+++ Assets/2_Scripts/QuestionSo.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class QuestionSo : ScriptableObject
 {
-    [TextArea(2,6)]
-    [SerializeField]string questionText = "여기에 질문을 적어주세요";
+    [TextArea(2, 6)]
+    [SerializeField] string questionText = "여기에 질문을 적어주세요";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerIndex = 0;
+
+
+    public string GetQuestion()
+    {
+        return questionText;
+    }
+
+    public string GetAnswers(int index)
+    {
+        return answers[index];
+    }
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
+    }
 }
---
+++ Assets/2_Scripts/Quiz.cs
@@ -0,0 +1,29 @@
+using TMPro;
+using UnityEngine;
+
+public class Quiz : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI questionText;
+    [SerializeField] QuestionSo questions;
+    [SerializeField] TextMeshProUGUI[] answerText;
+    void Start()
+    {
+        questionText.text = questions.GetQuestion();
+
+        //answerText[0].text = questions.GetAnswers(0);
+        //answerText[1].text = questions.GetAnswers(1);
+        //answerText[2].text = questions.GetAnswers(2);
+        //answerText[3].text = questions.GetAnswers(3);
+
+        for (int i = 0; i < answerText.Length; i++)
+        {
+        answerText[i].text = questions.GetAnswers(i);
+
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
